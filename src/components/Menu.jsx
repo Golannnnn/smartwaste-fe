080e3682bfa6944803051b244c8b9f89d0a2d9e9
Golannnnn <forburacc@protonmail.ch>
2023-05-31 15:31:09 +0300

    navbar finished

---
+++ src/components/Menu.jsx
@@ -0,0 +1,91 @@
+import {
+  Flex,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Text,
+  useDisclosure,
+  Link,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { useRef } from "react";
+import { IoMdCloudUpload } from "react-icons/io";
+import { BsFillFileEarmarkRuledFill } from "react-icons/bs";
+import { FaLightbulb } from "react-icons/fa";
+
+const Menu = () => {
+  const btnRef = useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button
+        ref={btnRef}
+        onClick={onOpen}
+        style={{
+          zIndex: 999,
+          backgroundColor: "#024731",
+        }}
+      >
+        <HamburgerIcon w={6} h={6} />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size="xs"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader fontSize="xl" borderBottomWidth="1px">
+            Menu
+          </DrawerHeader>
+
+          <DrawerBody fontSize="xl" mt={5} borderBottomWidth="1px">
+            <Flex align="center" gap={2} mb={7}>
+              <IoMdCloudUpload
+                style={{
+                  width: "30px",
+                  height: "30px",
+                }}
+              />
+              <Link href="#home">
+                <Text fontWeight={600}>Upload</Text>
+              </Link>
+            </Flex>
+            <Flex align="center" gap={2} mb={7}>
+              <BsFillFileEarmarkRuledFill
+                style={{
+                  width: "30px",
+                  height: "30px",
+                }}
+              />
+              <Link href="#rules">
+                <Text fontWeight={600}>Rules</Text>
+              </Link>
+            </Flex>
+            <Flex align="center" gap={2} mb={5}>
+              <FaLightbulb
+                style={{
+                  width: "30px",
+                  height: "30px",
+                }}
+              />
+              <Link href="#about">
+                <Text fontWeight={600}>About</Text>
+              </Link>
+            </Flex>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default Menu;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,31 @@
-import React from "react";
-import {
-  Flex,
-  Box,
-  Spacer,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  IconButton,
-} from "@chakra-ui/react";
-import "../assets/smartwaste_logo.png";
+import { Flex, Image, Text } from "@chakra-ui/react";
+import logoPng from "../assets/smartwaste_logo.png";
+import logoSvg from "../assets/smartwaste_logo.svg";
+import Menu from "./Menu";
+import { TbLocationFilled } from "react-icons/tb";
 
 const Navbar = () => {
+  const city = "Tel Aviv, Israel";
   return (
-    <div>
-      <Flex bg="#024731" color="#fff" align-item="center">
-        NavBar
+    <div id="home">
+      <Flex bg="#024731" color="#fff" align="center" justify="space-between">
+        <Flex align="center">
+          <Menu />
+          <Image
+            src={logoSvg}
+            alt="SmartWaste logo"
+            style={{
+              width: "120px",
+              height: "50px",
+            }}
+          />
+        </Flex>
+        <Flex align="center" gap={2} mr={3}>
+          <TbLocationFilled />
+          <Text fontSize="sm" fontWeight="bold">
+            {city}
+          </Text>
+        </Flex>
       </Flex>
     </div>
   );
