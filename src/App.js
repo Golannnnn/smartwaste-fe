40821226e1d57b961ec6a034d6a86fe4695e8ec9
Asil SD <aseel.sayyad1@gmail.com>
2023-05-31 14:22:59 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Home from "./componenets/Home";
 import Navbar from "./componenets/Navbar";
 import Rules from "./componenets/Rules";
@@ -11,6 +12,18 @@ function App() {
 
     </div>
   )
+=======
+import Map from "./components/Map";
+import Results from "./components/Results";
+
+function App() {
+  return (
+    <div className="App">
+      <Results />
+      <Map />
+    </div>
+  );
+>>>>>>> ea8350ed0c14ffc2d8a4eda4b4c6ac285da1b525
 }
 
 export default App;
---
+++ src/componenets/Map.jsx
@@ -0,0 +1,50 @@
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+import { Center, Heading } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import myLocationIcon from "../assets/dot.svg";
+const Map = () => {
+  const [myLocation, setMyLocation] = useState({
+    // tel aviv is the default location
+    lat: 43.6532,
+    lng: 34.7721026,
+  });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setMyLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    } else {
+      console.log("Geolocation is not supported for this Browser/OS.");
+    }
+  }, []);
+
+  const mapStyles = {
+    height: "400px",
+    width: "100%",
+  };
+
+  const binLocation = {
+    lat: 32.061993,
+    lng: 34.774991,
+  };
+
+  return (
+    <Center m={5} display="flex" flexDirection="column">
+      <Heading as="h1" size="2xl" mb={5}>
+        Map
+      </Heading>
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_KEY}>
+        <GoogleMap mapContainerStyle={mapStyles} zoom={14} center={myLocation}>
+          <MarkerF position={myLocation} icon={myLocationIcon} />
+          <MarkerF position={binLocation} />
+        </GoogleMap>
+      </LoadScript>
+    </Center>
+  );
+};
+
+export default Map;
